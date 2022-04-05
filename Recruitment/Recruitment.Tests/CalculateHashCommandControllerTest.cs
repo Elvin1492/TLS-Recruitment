4de04729fc246f0c3266442eabd619bbef08000d
Elvin-Mammadov <elvin.mammadov@altoros.com>
2022-04-05 12:56:29 +0400

    Adding test cases

--- Recruitment/Recruitment.Tests/CalculateHashCommandControllerTest.cs
+++ Recruitment/Recruitment.Tests/CalculateHashCommandControllerTest.cs
@@ -15,14 +15,16 @@ namespace Recruitment.Tests
 	public class CalculateHashCommandControllerTest : IntegrationTest
 	{
 		[Test]
-		[TestCase()]
-		public async Task Command_CalculateHash_HashedResult()
+		[TestCase("Elvin", "Hello!", "0B00C3A282E7F3BE33B86EC974C8B0CB0A26F4A953034FC5A49B109E6BDA91D5")]
+		[TestCase("Asma", "Hello!", "E3E00467F73BFEB48CF2C5E6754E490C736783B76635BA043BA9AEBDA726CAFE")]
+		[TestCase("Arzuman", "Hello!", "DE25D5FB07BA4409F525CB0DFACC7BE34A2E7A8B8A0AD442EC8ED58D7AC294B4")]
+		public async Task Command_CalculateHash_HashedResult(string login, string password, string assert)
 		{
 			// Arrange
 
 			var url = "http://WebApiTests.com/api/records";
 
-			var record = new CalculateHashCommand { Login = "Elvin", Password = "Hello!" };
+			var record = new CalculateHashCommand { Login = login, Password = password };
 
 			var request = new HttpRequestMessage { RequestUri = new Uri(url) };
 			request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -36,7 +38,7 @@ namespace Recruitment.Tests
 
 			// Assert
 			response.StatusCode.Should().Be(HttpStatusCode.OK);
-			result.Result.ToUpper().Should().Be("0B00C3A282E7F3BE33B86EC974C8B0CB0A26F4A953034FC5A49B109E6BDA91D5");
+			result.Result.ToUpper().Should().Be(assert);
 		}
 	}
 }
